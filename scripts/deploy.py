c2bc55310060f5e29be29060fec9920acd150639
pinajmr <pinajoma0@gmail.com>
2022-07-04 17:41:24 -0400

    deploy ready

---
+++ scripts/deploy.py
@@ -0,0 +1,49 @@
+from pyparsing import dict_of
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import config, PinaToken, TokenFarm, network
+from web3 import Web3
+
+KEPT_BALANCE = Web3.toWei(100, "ether")
+
+
+def deploy_token_farm_and_pina_token(front_end_update=False):
+    account = get_account()
+    pina_token = PinaToken.deploy({"from": account})
+    token_farm = TokenFarm.deploy(
+        pina_token.address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    tx = pina_token.transfer(
+        token_farm.address, pina_token.totalSupply() - KEPT_BALANCE, {"from": account}
+    )
+    tx.wait(1)
+    fau_token = get_contract("fau_token")
+    weth_token = get_contract("weth_token")
+
+    add_allowed_tokens(
+        token_farm,
+        {
+            pina_token: get_contract("dai_usd_price_feed"),
+            fau_token: get_contract("dai_usd_price_feed"),
+            weth_token: get_contract("eth_usd_price_feed"),
+        },
+        account,
+    )
+    # if update_front_end_flag:
+    #     update_front_end()
+    return token_farm, pina_token
+
+
+def add_allowed_tokens(token_farm, dict_of_allowed_token, account):
+    for token in dict_of_allowed_token:
+        token_farm.addAllowedTokens(token.address, {"from": account})
+        tx = token_farm.setPriceFeedContract(
+            token.address, dict_of_allowed_token[token], {"from": account}
+        )
+        tx.wait(1)
+    return token_farm
+
+
+def main():
+    deploy_token_farm_and_pina_token()
---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,84 @@
+from brownie import (
+    accounts,
+    network,
+    config,
+    Contract,
+    MockV3Aggregator,
+    MockDAI,
+    MockWETH,
+)
+
+
+NON_FORKED_LOCAL_BLOCKCHAIN_ENVIROMENTS = ["hardhat", "ganache", "development"]
+LOCAL_BLOCKCHAIN_ENVIROMENTS = NON_FORKED_LOCAL_BLOCKCHAIN_ENVIROMENTS + [
+    "mainnet-fork",
+    "mainnet-fork-dev",
+    "binance-fork",
+    "matic-fork",
+]
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "dai_usd_price_feed": MockV3Aggregator,
+    "fau_token": MockDAI,
+    "weth_token": MockWETH,
+}
+
+
+INITIAL_PRICE_FEED_VALUE = 2000000000000000000000
+DECIMALS = 18
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS:
+        return accounts[0]
+    if id:
+        return accounts.load(id)
+    return accounts.add(config["wallets"]["from_key"])
+
+
+def get_contract(contract_name):
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        try:
+            contract_address = config["networks"][network.show_active()][contract_name]
+            contract = Contract.from_abi(
+                contract_type._name, contract_address, contract_type.abi
+            )
+        except KeyError:
+            print(
+                f"{network.show_active()} address not found, perhaps you should add it to the config or deploy mocks?"
+            )
+            print(
+                f"brownie run scripts/deploy_mocks.py --network {network.show_active()}"
+            )
+    return contract
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_PRICE_FEED_VALUE):
+    """
+    if you want to deploy mocks to a testnet
+    """
+    print(f"The active network is {network.show_active()}")
+    print("Deploying mocks")
+    account = get_account()
+    print("Deploying Mock Price Feed")
+    mock_price_feed = MockV3Aggregator.deploy(
+        decimals,
+        initial_value,
+        {"from": account},
+    )
+    print(f"Deployed Mock Price Feed at {mock_price_feed.address}")
+    print("Deploying Mock DAI . . .")
+    mock_dai = MockDAI.deploy({"from": account})
+    print(f"Deployed Mock DAI at {mock_dai.address}")
+    print("Deploying Mock WETH . . .")
+    mock_weth = MockWETH.deploy({"from": account})
+    print(f"Deployed Mock WETH at {mock_weth.address}")
+    print(f"Mocks Deployed!")
